fac1817b26ed4e270eb61696acd24bdb70c891dc
Jaime Salas <jasalas@lafabril.com.ec>
2020-05-17 21:23:18 -0500

    Final del proyecto

--- vuetify/src/views/Home.vue
+++ vuetify/src/views/Home.vue
@@ -8,11 +8,12 @@
           full-width
           :min="minimo"
           :max="maximo"
+          @change="getDolar(fecha)"
           ></v-date-picker>
         </v-card>
         <v-card 
         color="error" dark >
-          <v-card-text class="display-1 text-center">650 {{fecha}}</v-card-text>
+          <v-card-text class="display-1 text-center">{{valor}}</v-card-text>
         </v-card>
       </v-col>
     </v-row>
@@ -26,19 +27,34 @@ import  axios from "axios";
 export default {
   data(){
     return{
-      fecha: '',
+      fecha: new Date().toISOString().substr(0,10),
       minimo: '1984',
-      maximo: new Date().toISOString().substr(0,10)
+      maximo: new Date().toISOString().substr(0,10),
+      valor: null
     }
   },
   methods:{
-    async getDolar(){
-      let datos= await axios.get('https://mindicador.cl/api/dolar')
-      console.log(datos);
+    async getDolar(dia){
+      let arrayFecha = dia.split(['-']);
+      let ddmmyy= arrayFecha[2]+'-'+arrayFecha[1]+'-'+arrayFecha[0];
+      try {
+        let datos= await axios.get(`https://mindicador.cl/api/dolar/${ddmmyy}`)
+        if (datos.data.serie.length >0){
+          this.valor = await datos.data.serie[0].valor;
+        }else{
+          this.valor= 'Sin Resultado'
+        }
+      } catch (error) {
+        console.log(error);
+      }
+      finally{
+
+      }
+      
     }
   },
   created(){
-    this.getDolar()
+    this.getDolar(this.fecha)
   },
   name: 'Home',
   components: {
